e9dc373624315ae7334c1f49f42089ee331af15a
2624431797 <2624431797@qq.com>
2020-05-29 17:39:09 +0800

    jsx

--- src/components/UpLoad/index.vue
+++ src/components/UpLoad/index.vue
@@ -1,12 +1,42 @@
 <template>
     <section class="upload-content">
-        
+        <el-tabs 
+            v-model="activeName" 
+            @tab-click="handlerClickTab"
+        >
+            <el-tab-pane 
+                label="用户管理" 
+                name="first"
+            >用户管理</el-tab-pane>
+        </el-tabs>
     </section>
 </template>
 
 <script>
 export default {
+    name: "UpLoad",
+    props: {
+        activeName: {
+            type: String,
+            default: ""
+        },
+        tabMapOptions: {
+            type: Array,
+            default: () => []
+        },
+    },
+    data(){
+        return{
 
+        }
+    },
+    methods: {
+        //点击tab
+        handlerClickTab(tab, event) {
+            console.log(tab, event)
+        },
+
+    }
 }
 </script>
 
